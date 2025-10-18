c6bd8783f5389e0e52da7ebff6f604b5411e3730
ANIME-40444 <animationzone35@gmail.com>
2025-10-18 19:23:19 +0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,22 @@
-// assets/js/firebase-config.js
-
-// Import Firebase modules
+// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// তোমার দেওয়া Firebase কনফিগ
 const firebaseConfig = {
   apiKey: "AIzaSyCJ5XnhKIM9isFqqlhZ0kAdG8DLUsR1TTY",
   authDomain: "anime-quiz-bd.firebaseapp.com",
   projectId: "anime-quiz-bd",
-  storageBucket: "anime-quiz-bd.appspot.com",
+  storageBucket: "anime-quiz-bd.firebasestorage.app",
   messagingSenderId: "888165764863",
   appId: "1:888165764863:web:33a9d7d14a26bc7c4b46ca",
   measurementId: "G-STX5S01SS0"
 };
 
-// Initialize Firebase
+// Firebase ইনিশিয়ালাইজেশন
 const app = initializeApp(firebaseConfig);
+
+// Auth এবং Firestore এক্সপোর্ট
 export const auth = getAuth(app);
 export const db = getFirestore(app);
